7d5dc242095c12fe512dd2359dd9b8e4bedf60ab
edwardoboh <obohedward@gmail.com>
2020-12-10 03:44:04 +0000

    On branch master  Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const ul = document.querySelector('#cafe-list')
+const form = document.querySelector("#add-cafe-form")
+
+//
+function renderData(doc){
+
+    let li = document.createElement("li")
+    li.setAttribute("data-id", doc.id)
+    let name = document.createElement("span")
+    let city = document.createElement("span")
+
+    name.textContent = doc.data().name
+    city.textContent = doc.data().city
+
+    li.appendChild(name)
+    li.appendChild(city)
+    ul.appendChild(li)
+    
+}
+
+// 
+async function getData() {
+    const data = await db.collection("cafes").get()
+    data.forEach(doc => {
+        // console.log(doc.data())
+        renderData(doc)
+    });
+}
+
+// 
+function handleForm() {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        db.collection("cafes").add({
+            name: form.name.value,
+            city: form.city.value
+        })
+        .then(() => {
+            console.log("Data successfully added to firestore")
+            form.name.value = ""
+            form.city.value = ""
+        })
+        .catch((err) => console.log("Unable to add data to firestore", err))
+    })
+}
+
+getData()
+handleForm()
+
